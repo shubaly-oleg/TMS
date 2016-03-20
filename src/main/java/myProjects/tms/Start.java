151e2739c5a55f1b396eb8dd467f69b52644f872
Shubalyi Oleg <shubaly.oleg@gmail.com>
2016-03-20 02:34:43 +0200

    start

---
+++ src/main/java/myProjects/tms/Start.java
@@ -0,0 +1,13 @@
+package myProjects.tms;
+
+/**
+ * Hello world!
+ *
+ */
+public class Start 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
